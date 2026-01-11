90f3e54352b5d9fdbdd314d8cd5df0279483041f
sayedmama772-jpg <sayedmama772@gmail.com>
2026-01-11 20:55:49 +0600

    Add keywords engine

---
+++ data/keywords.js
@@ -0,0 +1,50 @@
+// data/keywords.js
+export const WORDS = {
+  HELLO: [
+    "hi", "hello", "hlw", "hey", "yo",
+    "হাই", "হ্যালো", "salam", "slm", "assalamu"
+  ],
+
+  KEMON: [
+    "kemon", "kmn", "kemn", "kmon", "কেমন"
+  ],
+
+  ACHO: [
+    "acho", "aso", "achis", "achho", "asis",
+    "আছো", "আছ", "are", "r u"
+  ],
+
+  KI: [
+    "ki", "kee", "কি", "what", "wat"
+  ],
+
+  OBOSTHA: [
+    "obostha", "obosta", "obossta", "abastha",
+    "অবস্থা", "status", "condition"
+  ],
+
+  TUMI: [
+    "tumi", "tmi", "tum", "you", "u",
+    "তুমি", "আপনি", "তুই"
+  ],
+
+  KE: [
+    "ke", "kay", "কে", "who"
+  ],
+
+  ONLINE: [
+    "online", "active", "available", "on",
+    "অনলাইন", "একটিভ"
+  ],
+
+  SAYED: [
+    "sayed", "syed", "saied",
+    "ahmad", "ahmed",
+    "সায়েদ", "আহমাদ"
+  ],
+
+  HELP: [
+    "help", "sahajjo", "support",
+    "সাহায্য", "হেল্প"
+  ]
+};
